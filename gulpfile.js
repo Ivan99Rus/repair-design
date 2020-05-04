b57b507b16c3f21cb27a8f8e92e0467eea4595f4
Ivan <ivananapa@mail.ru>
2020-05-04 18:42:58 +0300

    added css minification task in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('hello', (done) => {
   console.log('Привет, мир');
   done();
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync.init({
-      server: {
-          baseDir: "./"
-      }
+    server: {
+      baseDir: "./"
+    }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('min-css', (done) => {
+  gulp.src('./css/*.css')
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest((file) => {
+      return file.base;
+    }));
+  done();
 });
