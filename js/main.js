20c2f58a2993b9884f97065321ba554b31df5cdf
Ivan <ivananapa@mail.ru>
2020-05-15 10:24:45 +0300

    feat: add up-button

--- js/main.js
+++ js/main.js
@@ -2,30 +2,38 @@ $(document).ready(() => {
   const modal = $('.modal'),
     modalBtn = $('[data-toggle="modal"]'),
     closeBtn = $('.modal__close');
-    scrollToTopBtn = $('.btn__scroll-to-top');
+    scrollToTopBtn = $('.button__scroll-to-top');
 
-    modalBtn.on('click', () => {
-      modal.toggleClass('modal--visible');
-    });
-    
-    closeBtn.on('click', () => {
-      modal.toggleClass('modal--visible');
-    });
-
-    $(document).on('keydown', (event) => {
-      if (event.key === 'Escape')
-        modal.removeClass('modal--visible');
-    });
-
-    $(document).on('click', (event) => {
-      let target = event.target;
-  
-      if ($(target).hasClass('modal--visible'))
-        modal.toggleClass('modal--visible');  
-    });
-
-  scrollToTopBtn.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
+  modalBtn.on('click', () => {
+    modal.toggleClass('modal--visible');
   });
+  
+  closeBtn.on('click', () => {
+    modal.toggleClass('modal--visible');
+  });
+
+  $(document).on('keydown', (event) => {
+    if (event.key === 'Escape')
+      modal.removeClass('modal--visible');
+  });
+
+  $(document).on('click', (event) => {
+    let target = event.target;
+
+    if ($(target).hasClass('modal--visible'))
+      modal.toggleClass('modal--visible');  
+  });
+
+   
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 400) {
+      $(scrollToTopBtn).fadeIn('slow');
+    } else { 
+      $(scrollToTopBtn).fadeOut('slow'); 
+    }
+    });
+
+    $(scrollToTopBtn).click(() => {
+      $('html, body').animate({scrollTop : 0}, 'slow');
+    });
 });
